4c38811c34fb9c206a2e3bc6db820dd04934db35
Karl Otepalu <k.otepalu@gmail.com>
2023-06-09 10:07:30 +0300

    test

--- Back-end/kysimus.js
+++ Back-end/kysimus.js
@@ -1,13 +1,11 @@
 function getRandomLocation() {
     var randomNum = Math.random();
     if (randomNum < 0.5){
-        var element = document.getElementById("textBubble");
-        element.style.marginLeft = "400px";
+        var element = document.getElementById("textbubble");
+    } else {
+        var element = document.getElementById("textbubble");
         element.style.webkitTransform = "scaleX(-1)";
         element.style.transform = "scaleX(-1)";
-    } else {
-        var element = document.getElementById("textBubble");
-        element.style.marginLeft = "1000px";  
     }
 }
 
