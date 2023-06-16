b65b721ddf1d829112d75212f61703b5ae98a181
Karl Otepalu <93727374+KOtepalu@users.noreply.github.com>
2023-06-16 09:42:08 +0300

    timer

--- Back-end/timer.js
+++ Back-end/timer.js
@@ -1,36 +1,31 @@
-//timer
-const beginTimer = document.getElementById("begin_btn");
-const timer = document.getElementById("timer");
-const finalAnswer = document.getElementById("finalAnswer");
+if (localStorage.getItem('timerValue')) {
 
-let seconds = 0;
-let minutes = 0;
-let timerInterval;
+    var timerValue = localStorage.getItem('timerValue');
+    startTimer(timerValue);
+  } else {
+    startTimer(20 * 60);
+  }
 
-function startTimer(){
-    timerInterval = setInterval(() => {
-        seconds++;
-    
-    if (seconds === 60){
-        seconds = 0;
-        minutes++;
-    }
-
-    timer.innerText = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+  function startTimer(duration) {
+    var timerDisplay = document.getElementById('timer');
+    var timer = duration;
+    var minutes, seconds;
+  
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+  
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+  
+      timerDisplay.textContent = minutes + ":" + seconds;
+  
+      localStorage.setItem('timerValue', timer);
+  
+      if (--timer < 0) {
+        clearInterval(timer); 
+        localStorage.removeItem('timerValue');
+        window.location.href = "results.php"; 
+      }
     }, 1000);
-
-    if(seconds === 00 && minutes == 20){
-        stopTimer();
-    }else if(finalAnswer == "clicked"){
-        stopTimer();
-    }
-}
-
-function stopTimer(){
-    clearInterval(timerInterval);
-
-    const elapsedTime = (minutes * 60) + seconds;
-    console.log(`Elapsed time: ${elapsedTime} seconds`);
-}
-
-beginTimer.addEventListener("click", startTimer);
+  }
