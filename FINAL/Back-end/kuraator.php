b29d2a921def38949d9bb4632a97f78bad02618e
Isabella <114983093+pebsisab@users.noreply.github.com>
2023-06-19 16:34:43 +0300

    kuraatori php koos analüütikaga

--- FINAL/Back-end/kuraator.php
+++ FINAL/Back-end/kuraator.php
@@ -5,12 +5,6 @@
     $dbname = "if22_DLGsimulaator";
 
     $comment_error = null;
-    
-/*     function delete_result($id){
-        echo $id;
-    } */
-
-    var_dump($_POST);
 
     if(isset($_POST["deleting"])){
         if(isset($_POST["to_delete"]) && !empty($_POST["to_delete"])){
@@ -27,6 +21,71 @@
             $conn->close();
         }
     }
+
+    //analytics, average result, average time
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $stmt = $conn->prepare("SELECT AVG(result_score), AVG(result_time) FROM User_Result");
+	echo $conn->error;
+    $stmt->bind_result($avg_score_db, $avg_time_db);
+    $stmt->execute();
+	echo $stmt->error;
+
+    $analytics_score = null; //average score
+    $analytics_time = null; //average time
+
+    if($stmt->fetch()){
+        $analytics_score = $avg_score_db;
+        $analytics_time = $avg_time_db;
+    }
+    $stmt->close();
+    $conn->close();
+
+    $analytics_time = round($analytics_time / 60);
+
+    //analytics, amount of participants
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $stmt = $conn->prepare("SELECT count(*) FROM User_Result");
+	echo $conn->error;
+    $stmt->bind_result($participants_db);
+    $stmt->execute();
+	echo $stmt->error;
+
+    $analytics_participants = null; //participants in total
+
+    if($stmt->fetch()){
+        $analytics_participants = $participants_db;
+    }
+    $stmt->close();
+    $conn->close();
+
+    //analytics, average age
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn->set_charset("utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $stmt = $conn->prepare("SELECT AVG(form_age) FROM Form");
+	echo $conn->error;
+    $stmt->bind_result($avg_age_db);
+    $stmt->execute();
+	echo $stmt->error;
+
+    $analytics_age = null; //participants in total
+
+    if($stmt->fetch()){
+        $analytics_age = $avg_age_db;
+    }
+    $stmt->close();
+    $conn->close();
+
 ?>
 
 <html>
@@ -92,6 +151,12 @@
                 </tbody>
             </table>
         </div>
+        <div class="sidecontent">
+            <div class="keskmine_vanus container"><p>Osalejate keskmine vanus:</p><br><?php echo $analytics_age; ?></div>
+            <div class="osalejad_kokku container"><p>Osalejaid kokku:</p><br><?php echo $analytics_participants; ?></div>
+            <div class="keskmine_tulemus container"><p>Keskmine tulemus:</p><br><?php echo $analytics_score; ?></div>
+            <div class="keskmine_aeg container"><p>Keskmine aeg:</p><br><?php echo $analytics_time; ?><p>minutit</p></div>
+        </div>    
     </div>
 </body>
 </html>
