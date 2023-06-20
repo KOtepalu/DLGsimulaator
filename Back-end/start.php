26cc989fcc34e5b8c14c6919d0bc50fea7cbf283
Karl Otepalu <93727374+KOtepalu@users.noreply.github.com>
2023-06-20 10:09:04 +0300

    start nupp uuendatud

--- Back-end/start.php
+++ Back-end/start.php
@@ -1,30 +1,17 @@
 <?php
+
+session_start();
 // Retrieve the form_id from the URL
-// error_reporting(E_ERROR | E_PARSE);
-if(isset($_GET['form_id'])){
-    $form_id = $_GET['form_id'];
-}
-if(isset($_GET['form_name'])){
-    $form_name = $_GET['form_name'];
-};
-if(isset($_GET['form_age'])){
-    $form_age = $_GET['form_age'];
-};
-if(isset($_GET['form_country'])){
-    $form_country = $_GET['form_country'];
-};
-if(isset($_GET['form_education'])){
-    $form_education = $_GET['form_education'];
-};
-if(isset($_GET['form_work'])){
-    $form_work = $_GET['form_work'];
-};
-if(isset($_GET['form_hobby'])){
-    $form_hobby = $_GET['form_hobby'];
-};
-if(isset($_GET['form_kids'])){
-    $form_kids = $_GET['form_kids'];
-};
+
+
+$new_form_id = $_SESSION['form_id'] ?? "";
+$form_name = $_SESSION['form_name'] ?? "";
+$form_age = $_SESSION['form_age'] ?? "";
+$form_country = $_SESSION['form_country'] ?? "";
+$form_education = $_SESSION['form_education'] ?? "";
+$form_work = $_SESSION['form_work'] ?? "";
+$form_hobby = $_SESSION['form_hobby'] ?? "";
+$form_kids = $_SESSION['form_kids'] ?? "";
 
 ?>
 
@@ -35,25 +22,27 @@ if(isset($_GET['form_kids'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Front-end/begin.css">
-    <script src="timer.js" defer></script>
+    <script src="timer.js"></script>
     <title>Start</title>
 </head>
-<body>    
-    <script>resetTimer();</script>
-    <div id="textcontainer">
-        <p>The interview is about to begin.</p>
-        <p>You have 20 minutes to answer to all the questions.</p>
-        <form action="index.php" method="GET">
-            <input type="hidden" name="form_id" value="<?php echo $form_id; ?>">
-            <input type="hidden" name="form_name" value="<?php echo $form_name; ?>">
-            <input type="hidden" name="form_age" value="<?php echo $form_age; ?>">
-            <input type="hidden" name="form_country" value="<?php echo $form_country; ?>">
-            <input type="hidden" name="form_education" value="<?php echo $form_education; ?>">
-            <input type="hidden" name="form_work" value="<?php echo $form_work; ?>">
-            <input type="hidden" name="form_hobby" value="<?php echo $form_hobby; ?>">
-            <input type="hidden" name="form_kids" value="<?php echo $form_kids; ?>">
-            <button id="button" type="submit">Begin</button>
-        </form>
-    </div>
+<body>
+  <div id="textcontainer">
+      <p>Welcome to the Interview Simulator!</p>
+      <p>Your form has been submitted successfully.</p>
+  
+    <form action="interview.php" method="POST">
+        <input type="hidden" name="form_id" value="<?php echo $form_id; ?>">
+        <input type="hidden" name="form_name" value="<?php echo $form_name; ?>">
+        <input type="hidden" name="form_age" value="<?php echo $form_age; ?>">
+        <input type="hidden" name="form_country" value="<?php echo $form_country; ?>">
+        <input type="hidden" name="form_education" value="<?php echo $form_education; ?>">
+        <input type="hidden" name="form_work" value="<?php echo $form_work; ?>">
+        <input type="hidden" name="form_hobby" value="<?php echo $form_hobby; ?>">
+        <input type="hidden" name="form_kids" value="<?php echo $form_kids; ?>">
+        <input type="hidden" name="form_id" value="<?php echo $new_form_id; ?>">
+        <button id="button" type="submit">START</button>
+    </form>
+  </div>
+  <script>resetTimer();</script>
 </body>
 </html>
